7aac3802f3f7f3f838f1e34478345d4bd6cc4258
Oli Hock <oliver.hock@gmail.com>
2024-02-06 19:12:03 +0100

    feat: Add spring-boot-starter-webflux dependency in pom.xml
    chore: Update ItemsControllerIT to use WebTestClient instead of TestRestTemplate

--- src/test/java/de/ithoc/springboot/problemdetail/item/ItemsControllerIT.java
+++ src/test/java/de/ithoc/springboot/problemdetail/item/ItemsControllerIT.java
@@ -2,54 +2,51 @@ package de.ithoc.springboot.problemdetail.item;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.net.URI;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ItemsControllerIT {
 
     @Autowired
-    TestRestTemplate restTemplate;
+    WebTestClient webTestClient;
 
     @Test
-    void getItemExceptionAdvice() {
+    void shouldRespondProblemDetail() {
 
         String itemName = "Item 1";
         long timestamp = System.currentTimeMillis() - 1000;
 
-        ResponseEntity<ProblemDetail> responseEntity =
-                restTemplate.getForEntity("/items/exception-advice/" + itemName, ProblemDetail.class);
-
-        if (responseEntity.getStatusCode().is4xxClientError()) {
-            ProblemDetail problemDetail = (ProblemDetail) responseEntity.getBody();
-
-            assertEquals(HttpStatus.CONFLICT, responseEntity.getStatusCode());
-            assert problemDetail != null;
-            assertEquals("Item already exists", problemDetail.getTitle());
-            assertEquals("Item 'Item 1' already available.", problemDetail.getDetail());
-            assertEquals(
-                    URI.create("http://localhost:18084/api/items/exception-advice/item-already-exists"),
-                    problemDetail.getType());
-            assertEquals(URI.create("/api/items/exception-advice"), problemDetail.getInstance());
-            assertTrue(
-                    timestamp < (Long) Objects.requireNonNull(problemDetail.getProperties()).get("timestamp")
-            );
-            assertEquals(
-                    "Electronics",
-                    Objects.requireNonNull(problemDetail.getProperties()).get("category")
-            );
-        } else {
-            fail();
-        }
+        webTestClient.get()
+                .uri("/items/exception-advice/" + itemName)
+                .exchange()
+                .expectStatus().is4xxClientError()
+                .expectHeader().contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .expectBody(ProblemDetail.class)
+                .value(problemDetail -> {
+                    assertEquals(HttpStatus.CONFLICT.value(), problemDetail.getStatus());
+                    assertEquals("Item already exists", problemDetail.getTitle());
+                    assertEquals("Item 'Item 1' already available.", problemDetail.getDetail());
+                    assertEquals(
+                            URI.create("http://localhost:18084/api/items/exception-advice/item-already-exists"),
+                            problemDetail.getType());
+                    assertEquals(URI.create("/api/items/exception-advice"), problemDetail.getInstance());
+                    assertTrue(timestamp < (Long) Objects.requireNonNull(
+                            problemDetail.getProperties()).get("timestamp"));
+                    assertEquals(
+                            "Electronics",
+                            Objects.requireNonNull(problemDetail.getProperties()).get("category")
+                    );
+                });
     }
 
 }
